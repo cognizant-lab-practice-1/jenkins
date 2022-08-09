f002c2667d88e93a9ceccf5a6e207812213f9545
zohairawan <zawan@luc.edu>
2022-08-09 12:39:29 -0700

    Added bucket name

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 terraform {
   backend "s3" {
-    bucket         = "<your bucket here>"
+    bucket         = "cog-lab"
     key            = "<your statefile here>"
     region         = "us-east-1"
   }
