16a2b0dc80e07467e8be07c314c434048cb06eed
zohairawan <zawan@luc.edu>
2022-08-09 19:58:03 -0700

    Updated values in main and and added keypair

--- main.tf
+++ main.tf
@@ -1,12 +1,12 @@
 provider "aws" {
-  region  = "us-east-1"
+  region  = "us-west-1"
 }
 
 terraform {
   backend "s3" {
     bucket         = "cog-lab"
-    key            = "<your statefile here>"
-    region         = "us-east-1"
+    key            = "jenkins/terraform.tfstate"
+    region         = "us-west-1"
   }
 }
 
